2beaa09569c1b83baf6ef0b082fe408e51a4ecb6
Chris Haas <cwhaas98@gmail.com>
2021-02-08 09:28:45 -0500

    Child passes

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,7 +1,7 @@
 class Child < ApplicationRecord
     # Relationships
     has_many :chores
-    has_many :tasks, through :chores
+    has_many :tasks, through: :chores
 
     # Validations
     validates_presence_of :first_name , :last_name
