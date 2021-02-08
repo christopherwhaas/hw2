3955a6959bc189c74a1da249a9467c2f026d3451
Chris Haas <cwhaas98@gmail.com>
2021-02-08 09:23:57 -0500

    Child Created

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,7 +1,22 @@
 class Child < ApplicationRecord
-    #Validations
+    # Relationships
     has_many :chores
     has_many :tasks, through :chores
+
+    # Validations
+    validates_presence_of :first_name , :last_name
+    
+
+    # Scope
+    scope :alphabetical, -> { order('last_name, first_name') }
+
+    scope :active, -> { where(active: true) }
+
+    
+    # Methods
+    def name
+        first_name + " " + last_name
+    end
 
     
 end
